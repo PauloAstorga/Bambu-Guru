6e2c8ef3e5c80bca5037914643cd0d64a8a0433c
PauloAstorga <pauloastorga.docs@gmail.com>
2021-11-21 17:19:45 -0300

    Agregada responsividad del header nav

---
+++ resources/js/main.js
@@ -0,0 +1,30 @@
+/*Menu show and hidden*/
+
+const navMenu = document.getElementById('nav-menu'),
+    navToggle = document.getElementById('nav-toggle'),
+    navClose = document.getElementById('nav-close')
+
+/*Menu show*/
+if (navToggle) {
+    navToggle.addEventListener('click', () =>{
+        navMenu.classList.add('show-menu')
+    })
+}
+
+/*Menu hidden*/
+if (navClose) {
+    navClose.addEventListener('click', () =>{
+        navMenu.classList.remove('show-menu')
+    })
+}
+
+/*Remove menu mobile*/
+
+const navLink = document.querySelectorAll('.nav__link')
+
+function linkAction(){
+    const navMenu = document.getElementById('nav-menu')
+
+    navMenu.classList.remove('show-menu')
+}
+navLink.forEach( n => n.addEventListener('click', linkAction))
